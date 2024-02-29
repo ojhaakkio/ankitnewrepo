fce180ca4d54a8cc5133510ebda979acc6b412e4
ojhaakkio <157199909+ojhaakkio@users.noreply.github.com>
2024-02-29 20:44:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6520263954:AAGlpa8mPjhyfSdH789Rjy9sBlnBWsZRZHM",
+             bot_token= "7129072666:AAEcOXlkZSURs2XXVCuxOjeLssCuP7bI4Lk",
              api_id=20346550,
              api_hash= "bc79c3bea7a626887bdc0871eecf0327")
 
